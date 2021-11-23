d0aa9867e6ba31acbc8fced02c3cb15467661c87
BT-8714 <zeno8714@naver.com>
2021-11-23 14:55:14 +0900

    fixed

--- sensor/script/sensor_node.py
+++ sensor/script/sensor_node.py
@@ -10,7 +10,7 @@ rate = rospy.Rate(1)
 while not rospy.is_shutdown():
     msg.timestamp = rospy.get_rostime()
     second = msg.timestamp.secs
-    msg.pose = Pose2D(x=second*2, y=second%4, theta=second+6)
+    msg.pose = Pose2D(x=second%3, y=second%5, theta=second%7)
     pub.publish(msg)
     print "sensor:", msg.timestamp.secs%40, msg.pose.x, msg.pose.y, msg.pose.theta
     rate.sleep()
