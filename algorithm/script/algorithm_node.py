3be18b10f69a5426fd13ab27f8ec010969f8da1b
BT-8714 <zeno8714@naver.com>
2021-11-23 14:48:24 +0900

    Modify and Change

--- algorithm/script/algorithm_node.py
+++ algorithm/script/algorithm_node.py
@@ -3,7 +3,7 @@ import rospy
 from topic_custom.msg import TimePose
 
 def callback(msg):
-    print "subscribe:", msg.timestamp.secs%50, msg.pose.x, msg.pose.y, msg.pose.theta
+    print "algorithm:", msg.timestamp.secs%40, msg.pose.x, msg.pose.y, msg.pose.theta
 
 rospy.init_node('algorithm_node')
 sub = rospy.Subscriber('topic_msg', TimePose, callback)
--- sensor/script/sensor_node.py
+++ sensor/script/sensor_node.py
@@ -10,7 +10,7 @@ rate = rospy.Rate(1)
 while not rospy.is_shutdown():
     msg.timestamp = rospy.get_rostime()
     second = msg.timestamp.secs
-    msg.pose = Pose2D(x=second%2, y=second%4, theta=second%8)
+    msg.pose = Pose2D(x=second*2, y=second%4, theta=second+6)
     pub.publish(msg)
-    print "publish:", msg.timestamp.secs%50, msg.pose.x, msg.pose.y, msg.pose.theta
+    print "sensor:", msg.timestamp.secs%40, msg.pose.x, msg.pose.y, msg.pose.theta
     rate.sleep()
